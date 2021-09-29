9084fce274d0a9f7fa6a0c51b9f8d5a84be33018
JiangYan0426 <2571560168@qq.com>
2021-09-29 17:03:19 +0800

    github搜索案例_axios发送请求

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+    search=()=>{
+        // 获取用户的输入(连续解构赋值+重命名)
+        const {keyWordElement:{value:keyWord}}=this
+        console.log(keyWord);
+        // 发送网络请求
+        axios.get(`/api1/search/users?q=${keyWord}`).then(
+            response=>{console.log('成功了',response.data);},
+            error=>{console.log('失败了',error);}
+        )
+
+
+
+    }
     render() {
         return (
             <section className="jumbotron">
-                <h3 className="jumbotron-heading">Search Github Users</h3>
+                <h3 className="jumbotron-heading">搜索github用户</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+                    {/* ref回调 */}
+                    <input ref={c=>this.keyWordElement=c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+                    <button onClick={this.search}>Search</button>
                 </div>
             </section>
         )
