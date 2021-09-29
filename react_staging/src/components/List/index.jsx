6a789db81926311cc814a4244b197843e92ad169
JiangYan0426 <2571560168@qq.com>
2021-09-29 15:19:30 +0800

    github搜索案例_静态组件

---
+++ react_staging/src/components/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class List extends Component {
+    render() {
+        return (
+            <div className="row">
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+          </div>
+        )
+    }
+}
