685299d405d049f3ba892a7984b820d387b6e8c7
JiangYan0426 <2571560168@qq.com>
2021-09-30 10:46:30 +0800

    github搜索案例_展示数据

--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -4,37 +4,19 @@ import './index.css'
 export default class List extends Component {
     render() {
         return (
-            <div className="row">
-            <div className="card">
-              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-                <img alt="head_portrait" src="https://img2.baidu.com/it/u=3864578316,873279422&fm=26&fmt=auto" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
+          <div className="row">
+            {
+              this.props.users.map((userObj)=>{
+                return(
+                  <div key={userObj.id} className="card">
+                    <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                      <img alt="head_portrait" src={userObj.avatar_url} style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">{userObj.login}</p>
+                  </div>
+                )
+              })
+            }
           </div>
         )
     }
--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -5,15 +5,13 @@ export default class Search extends Component {
     search=()=>{
         // 获取用户的输入(连续解构赋值+重命名)
         const {keyWordElement:{value:keyWord}}=this
-        console.log(keyWord);
         // 发送网络请求
         axios.get(`/api1/search/users?q=${keyWord}`).then(
-            response=>{console.log('成功了',response.data);},
+            response=>{
+                this.props.saveUsers(response.data.items)
+            },
             error=>{console.log('失败了',error);}
         )
-
-
-
     }
     render() {
         return (
